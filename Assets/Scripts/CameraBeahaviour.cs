a9858e14097dcd34fba9188f0ba22c0bf5ad28d5
Eugene <zlysanskiy@gmail.com>
2020-12-02 23:25:50 +0300

    Standart
    Standart

---
+++ Assets/Scripts/CameraBeahaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CameraBeahaviour : MonoBehaviour
+{
+    [Header("GameObject")]
+    public Transform _gameObject;
+    [Header("Camera position restrictions")]
+    public float minY;
+    public float maxY;
+    public float minX;
+    public float maxX;
+
+    void Update()
+    {
+        UpdateCameraPosition();
+    }
+
+    // Изменяем позицию камеры на экране
+  void UpdateCameraPosition()
+    {
+        try
+        {
+            transform.position = new Vector3(
+                // Положение игрового объекта, за которым мы двигаемся
+                Mathf.Clamp(_gameObject.position.x, minX, maxX),
+                Mathf.Clamp(_gameObject.position.y, minY, maxY),
+                // Положение камеры z должно оставать неизменным 
+                transform.position.z // (если камеры куда-то проваливается, заменить на, например, -10)
+              );
+        }
+        catch (Exception error)
+        {
+           
+            Debug.LogError(error);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    /**
+   ** Ускорение игрока
+   **/
+    [Header("Player velocity")]
+    // Ось Ox
+    public int xVelocity = 5;
+    // Ось Oy
+    public int yVelocity = 8;
+    [SerializeField] private LayerMask ground;
+
+    // Физическое поведение (тело) объекта
+    private Rigidbody2D rigidBody;
+
+    // Коллайдер, проверка на столкновения
+    private Collider2D coll;
+
+    // Start (https://docs.unity3d.com/ScriptReference/MonoBehaviour.Start.html)
+    private void Start()
+    {
+        // Получаем доступ к Rigidbody2D объекта Player
+        rigidBody = gameObject.GetComponent<Rigidbody2D>();
+        coll = gameObject.GetComponent<Collider2D>();
+    }
+
+    private void Update()
+    {
+        updatePlayerPosition();
+    }
+
+    // Обновляем местоположение игрока
+    private void updatePlayerPosition()
+    {
+        // Получаем значение ввода горизонтального перемещение
+        float moveInput = Input.GetAxis("Horizontal");
+        // Получаем значение ввода прыжка
+        float jumpInput = Input.GetAxis("Jump");
+
+        // Значения xVelocity, yVelocity можно задать через инспектор
+
+        if (moveInput < 0)
+        { // Движ влево
+            rigidBody.velocity = new Vector2(-xVelocity, rigidBody.velocity.y);
+        }
+        else if (moveInput > 0)
+        { // Движ вправо
+            rigidBody.velocity = new Vector2(xVelocity, rigidBody.velocity.y);
+        }
+        else if (coll.IsTouchingLayers(ground))
+        {
+            rigidBody.velocity = Vector2.zero; // Лично меня дико бесит инерция вбок при приземлении персонажа, отключаем
+        }
+
+        if (jumpInput > 0 && coll.IsTouchingLayers(ground))
+        { //Тип прыгает, если стоит на земле
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, yVelocity);
+        }
+    }
+}
