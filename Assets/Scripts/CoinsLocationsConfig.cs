f949e7cc1addf5e27dd62fdfdb2a68b2bc4c4849
Eugene <zlysanskiy@gmail.com>
2020-12-07 00:00:45 +0300

    More

---
+++ Assets/Scripts/CoinsLocationsConfig.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Создаём новый пункт меню "Coin Locations Config" для создания новых конфигов
+[CreateAssetMenu(menuName = "Coin Locations Config")]
+public class CoinsLocationsConfig : ScriptableObject
+{
+    [SerializeField] GameObject coinPrefab;
+    [SerializeField] GameObject coinLocations;
+
+    // Префаб coin
+    public GameObject coin
+    {
+        get
+        {
+            return coinPrefab;
+        }
+    }
+
+    //  Точки спауна монет
+    public List<Vector3> coinsSpawnPoints
+    {
+        get
+        {
+            return ExtractPositionPoints();
+        }
+    }
+
+    // "Распаковываем" точки спауна врагов из контейнера
+    private List<Vector3> ExtractPositionPoints()
+    {
+        var locations = new List<Vector3>();
+
+        foreach (Transform child in coinLocations.transform)
+        {
+            locations.Add(child.transform.transform.position);
+        }
+
+        return locations;
+    }
+}
---
+++ Assets/Scripts/CoinsSpawnerBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinsSpawnerBehaviour : MonoBehaviour
+{
+    [SerializeField] CoinsLocationsConfig coinLocationsConfig;
+
+    void Start()
+    {
+        // Вызываем метод SpawnCoins при инициации спаунера
+        SpawnCoins();
+    }
+
+    // Спауним монеты
+    private void SpawnCoins()
+    {
+        // Получаем список локаций для спауна монет
+        List<Vector3> points = coinLocationsConfig.coinsSpawnPoints;
+
+        // Проходимся по точкам спауна ним циклом
+        foreach (Vector3 spawnPoint in points)
+        {
+            // Размещаем монеты на карте
+            Instantiate(
+              // Префаб монеты
+              coinLocationsConfig.coin,
+              // Точка спауна
+              spawnPoint,
+              // Ассоциируем созданный объект с игровым миром
+              Quaternion.identity
+            );
+
+            /**
+            * Данный шаг может вам не понадобиться. По какой то непонятной мне причине,
+            * координата z точек спауна периодически меняется, хотя должна оставаться нулём.
+            * Если у вас такая же проблема, просто заменит код в цикле на этот
+            *
+            * var position = spawnPoint;
+            * position.z = 0;
+            *
+            *  Instantiate(coinLocationsConfig.coin, position, Quaternion.identity);
+            **/
+        }
+    }
+}
